d2c80f903cd70f263e242e1119a1a6bcedc1f893
Alex Oughton <alex@oughton.me.uk>
2024-12-11 11:52:57 -0500

    Disable debug logging

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 go build
+echo "If pushing:"
+echo "docker build -t alexoughton/kube-plex:{VERSION_NUMBER} ."
+echo "docker tag alexoughton/kube-plex:{VERSION_NUMBER} alexoughton/kube-plex:latest"
+echo "docker push alexoughton/kube-plex:{VERSION_NUMBER}"
+echo "docker push alexoughton/kube-plex:latest"
--- main.go
+++ main.go
@@ -97,8 +97,6 @@ func rewriteArgs(in []string) {
 		switch v {
 		case "-progressurl", "-manifest_name", "-segment_list":
 			in[i+1] = strings.Replace(in[i+1], "http://127.0.0.1:32400", pmsInternalAddress, 1)
-		case "-loglevel", "-loglevel_plex":
-			in[i+1] = "debug"
 		}
 	}
 }
